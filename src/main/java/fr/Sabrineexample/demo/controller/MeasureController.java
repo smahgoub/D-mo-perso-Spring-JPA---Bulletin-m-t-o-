2ba4e94b9a8a19f6a5b5476923be50f7fa7e3c04
Sabrine <mahgoub22@gmail.com>
2021-06-07 17:02:58 +0200

    third commit

--- src/main/java/fr/Sabrineexample/demo/controller/MeasureController.java
+++ src/main/java/fr/Sabrineexample/demo/controller/MeasureController.java
@@ -1,63 +1,70 @@
-package fr.Sabrineexample.demo.controller;
+package com.example.demojpacity.controller;
 
 import fr.Sabrineexample.demo.model.Measure;
-import fr.Sabrineexample.demo.repository.MeasureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import fr.Sabrineexample.demo.repository.MeasureRepository;
 
-import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-
-@RequestMapping("/API")
+@RequestMapping("/api")
+@CrossOrigin("*")
 public class MeasureController {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Autowired
-    MeasureRepository measureRepository;
+    private MeasureRepository measureRepository;
 
-    @RequestMapping(value = "/measures/last", method = RequestMethod.GET)
-    public Measure last(@RequestParam("measure-type") String measureType) {// permet de récupérer ce qu'il y a après le ? measure-type
-        System.out.println("Measure type : " + measureType);
-        Measure resultat = null;
-        return resultat;
-    }
-    @RequestMapping(value = "/measures/top", method = RequestMethod.GET)
-    public Measure top(@RequestParam("measure-type") String measureType) {// permet de récupérer ce qu'il y a après le ? measure-type
-        System.out.println("Measure type : " + measureType);
-        Measure resultat2 =null;
-        return resultat2;
-    }
-    @RequestMapping(value = "/measures", method = RequestMethod.GET)
-    public List<Measure> table(){//@RequestParam("measure-type") String measureType, @RequestParam("start-date") String startDate, @RequestParam("end-date") String endDate) {
-
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Measure> findAll(){
         return measureRepository.findAll();
     }
+    // requête localhost:8090/villes/sub?measure-type=MaMesure
+    @RequestMapping(value = "/sub"
+            ,method = RequestMethod.GET)
+    public String testSub(@RequestParam("measure-type") String measureType,
+                          @RequestParam(value = "start-date", required = false) LocalDateTime begin,
+                          @RequestParam(value = "end-date", required = false) LocalDateTime end){
+        String res=  "measure-type: "+measureType+"\n"+"begin: "+ begin+"\n"+"end: "+ end;
+        System.out.println(res);
+        return "<H1> Test sub</H1>\n"+res;
+    }
 
-    @RequestMapping(value = "/measures", method=RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, consumes="application/x-www-form-urlencoded")
     @ResponseStatus(HttpStatus.CREATED)
-    public Measure createMesure (@RequestBody Measure measure) {
+    public Long createForm(Measure resource) {
+        Measure res= measureRepository.save(resource);
+        return res.getId();
 
-                return measure;
     }
+    @Transactional
+    @RequestMapping(method = RequestMethod.POST, consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long createJson(@RequestBody Measure resource) {
+        Measure res= measureRepository.save(resource);
+        res.setType(res.getType()+" modified after save !");
+        System.err.println("managed ?:"+entityManager.contains(res));
+        return res.getId();
+    }
+
+    @RequestMapping(value = "/{identifiant}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("identifiant") Long id){
+        measureRepository.deleteById(id);
+        System.out.println("Suppression de l'entité avec l'id :" +id);
+    }
+    @RequestMapping(value = "/{identifiant}", method = RequestMethod.GET)
+    public Optional<Measure> readOne(@PathVariable("identifiant") Long id){
+        return measureRepository.findById(id);
+    }
+
 }
-
-
-    /* @RequestMapping(value = "/sub", method = RequestMethod.GET) // Si la requête qui arrive est sur /API/sub, qu'elle est GET, il faut appliquer la méthode testSub
-    public String testSub() {
-        return "<H1> Test sub </H1>";
-    }
-    // Méthode qui va etre executé pour la requête GET sur le chemin /API/sub
-
-    @RequestMapping(method = RequestMethod.POST) // Si la requête qui arrive est sur /API, qu'elle est POST, il faut appliquer la méthode create
-
-    @ResponseStatus(HttpStatus.CREATED) // Quand tout s'est bien passé (OK), on execute la méthode create indiquant que la création s'est bien passé (coté utilisateur)
-    // Permet de customiser la réponse automatique
-
-    public Long create(@RequestBody long ressource) { // Passage entre ce qui circule dans la requete et les objets Java
-        System.out.println("ressource :" + ressource);
-        return 5L; // Retourne 1 de type Long
-    }
-}
-
-*/
